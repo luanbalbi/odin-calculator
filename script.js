b96d9e4dd2f3c6de9878ae65a1819d2840daa64d
Luan Balbi <49569950+luanbalbi@users.noreply.github.com>
2024-02-27 23:38:45 -0300

    Update buttons features to work properly

--- script.js
+++ script.js
@@ -1,39 +1,101 @@
 const numbers = /[0-9]/;
 const operators = /\+|\-|\*|\//
-let number1 = 0;
-let number2 = '';
-let operator = '';
+var number1 = '';
+var number2 = '';
+var operator = '';
+let lastButton = '';
 
 const display = document.querySelector(".display");
+const minidisplay = document.querySelector(".minidisplay");
 const button = document.querySelectorAll(".calc-row > button");
 
  button.forEach((btn) => {
     btn.addEventListener("click", () => {
         if (numbers.test(btn.textContent)) {
-            if (display.textContent == '0') display.textContent = '';
+            if (display.textContent == '0' || display.textContent === 'ERROR'){
+                    display.textContent = '';
+                }
+            if (lastButton === '=') {
+                display.textContent = '';
+                minidisplay.textContent = '';
+                number1 = '';
+                number2 = '';
+                operator = '';
+            }
+            if (!operator) {
+                number1 += btn.textContent;
+            }
+            if (operator && number1) {
+                if (!number2) {
+                    display.textContent = '';
+                }
+            number2 += btn.textContent;
+            }
             display.textContent += btn.textContent;
         }
         if (operators.test(btn.textContent)) {
             if (btn.textContent === '+/-') {
                 display.textContent = +display.textContent * (-1);
             } else {
-                display.textContent = 0;
-                if (operator != '') {
-                    number2 = display.textContent;
-                    console.log(operate(+number1, +number2, operator));
+                if (lastButton === '=') {
+                    number2 = '';
+                }
+                if (operator === '/' && number2 == '0'){
+                    minidisplay.textContent = '';
+                    number1 = '';
+                    number2 = '';
+                    operator = '';
+                    display.textContent = 'ERROR';
+                }
+                operator = btn.textContent;
+                if (number1 && number2) {
+                        display.textContent = operate(+number1, +number2, operator);
+                        number1 = display.textContent;
+                        number2 = '';
+                }
+                if (number1 && operator) {
+                    minidisplay.textContent = +number1 + operator;
+                }
+                if (!number1 && !number2) {
+                    operator = '';
+            }
+            }
+        }
+        if (btn.textContent === '=') {
+            if (display.textContent === 'ERROR'){
+                display.textContent = '0';
+            }
+            if (number1 && !operator) {
+                minidisplay.textContent = +number1 + '=';
+            }
+            if (number1 && operator) {
+                if (!number2) {
+                    number2 = number1;
+                }              
+                if (operator === '/' && number2 == '0'){
+                    minidisplay.textContent = '';
+                    number1 = '';
+                    number2 = '';
+                    operator = '';
+                    display.textContent = 'ERROR';
+                } else {
+                    minidisplay.textContent = +number1 + operator + +number2 + '=';
                     display.textContent = operate(+number1, +number2, operator);
                     number1 = display.textContent;
                 }
-                operator = btn.textContent;
-                if (number1 === '') {number1 = display.textContent};
             }
         }
-        if (btn.textContent === 'CE') {
+        if (btn.textContent === 'C') {
             display.textContent = '0';
-            number1 = 0;
+            minidisplay.textContent = '';
+            number1 = '';
             number2 = '';
             operator = '';
         }
+        if (btn.textContent === 'CE') {
+            display.textContent = '0';
+        }
+        lastButton = btn.textContent;
     });
 });
 
@@ -52,6 +114,8 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
     return number1 / number2;
 }
+
+
 
 function operate(number1, number2, operator) {
     switch (operator) {
